4611e1e771e1e7baf6f14ffdd3f27c6d45f575fb
Your Name <you@example.com>
2023-02-07 00:13:24 +0300

    Web_Sem3_HW

---
+++ 1.js
@@ -0,0 +1,5 @@
+//  градусы Фаренгейта = (9 / 5) * градусы Цельсия + 32
+const cells_deg = +prompt("Введите значение градусов Цельсия")
+let forengeit_deg = Math.round(((9 / 5)*cells_deg+32)*100)/100
+//forengeit_deg = Math.round(forengeit_deg.math)
+alert (forengeit_deg)
---
+++ 2.js
@@ -0,0 +1,6 @@
+function greeting(name_user){
+console.log(`Привет ${name_user}!`);
+}
+
+const name_user = prompt('Как Вас зовут?');
+greeting(name_user)
